271b8a980291d089ef25dff2a49c38859897d4d7
Tony Fettes <tonyfettes@tonyfettes.com>
2021-06-23 21:29:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# C++ project template based on xmake build system for (Joint-Online-Judge)[https://joj.sjtu.edu.cn/]
+
+## Usage
+
+Please refer to `xmake.lua`.
---
+++ xmake.lua
@@ -0,0 +1,114 @@
+-- There is no need to have release mode, right?
+add_rules("mode.debug", "mode.coverage", "mode.valgrind")
+
+-- dependencies for unit test
+add_requires("conan::boost-ext-ut/1.1.8")
+
+-- rules to package source files as an archive to be submitted on joj
+rule("joj")
+  on_package(function (target)
+
+    -- If files that needed to be uploaded are not specified, issues an error.
+    assert(target:values("joj.files") ~= nil, "joj.files must be specified, e.g. add_values(\"joj.files\", \"src/main.cpp\")")
+    assert(target:values("joj.format") ~= nil, "joj.format must be specified, e.g. set_values(\"joj.format\", \"zip\")")
+
+    local archive_format = target:values("joj.format")
+    local archive_name = target:name() .. "." .. archive_format
+    local archive_command = nil
+    local archive_param = nil
+
+    -- Hardcode the command name and parameters each format corresponds to
+    if archive_format == "zip" then
+      archive_command = "zip"
+      archive_param = nil
+    elseif archive_command == "tar" then
+      archive_command = "tar"
+      archive_param = "-cvf"
+    end
+
+    -- Check if user passes in joj.archivedir, if not, its value fallbacks to
+    -- "uploads".
+    local archive_dir = nil
+    if not target:values("joj.archivedir") then
+      archive_dir = "uploads"
+    else
+      archive_dir = target:values("joj.archivedir")
+    end
+
+    -- Concatnating archive_command/param/name into one string
+    local archive_command = archive_command .. " "
+    if archive_param then
+      archive_command = archive_command .. archive_param .. " "
+    end
+    archive_command = archive_command .. archive_name
+
+    -- Check if user passed in joj.sourcedir, if not, treat project root as
+    -- sourcedir.
+    if not target:values("joj.sourcedir") then
+      os.execv(archive_command, target:values("joj.files"));
+      if os.exists(archive_dir) then
+        os.mkdir(archive_dir)
+      end
+      os.mv(archive_name, archive_dir);
+    else
+      project_root = os.cd(target:values("joj.sourcedir"))
+      os.execv(archive_command, target:values("joj.files") );
+      os.cd(project_root)
+      if os.exists(archive_dir) then
+        os.mkdir(archive_dir)
+      end
+      os.mv(target:values("joj.sourcedir") .. "/" .. archive_name, archive_dir .. "/")
+    end
+  end)
+
+  after_clean(function (target)
+    assert(target:values("joj.files") ~= nil, "joj.files must be specified, e.g. add_values(\"joj.files\", \"src/main.cpp\")")
+    assert(target:values("joj.format") ~= nil, "joj.format must be specified, e.g. set_values(\"joj.format\", \"zip\") ")
+    local archive_format = target:values("joj.format")
+    local archive_name = target:name() .. "." .. archive_format
+    local archive_dir = nil
+    if not target:values("joj.archivedir") then
+      archive_dir = "uploads"
+    else
+      archive_dir = target:values("joj.archivedir")
+    end
+    if os.exists(archive_dir .. "/" .. archive_name) then
+      os.rm(archive_dir .. "/" .. archive_name)
+    end
+  end)
+
+target("example")
+  set_default(true)
+  set_kind("binary")
+
+  -- I was to that I'm allowed to use C++11
+  set_languages("cxx11")
+
+  -- This equivalent to -Wall -Wextra -Werror
+  set_warnings("allextra", "error")
+
+  -- It seems that TAs will assume you run your binary under the root directory
+  -- of your project.
+  set_rundir("$(projectdir)")
+  add_files("src/*.cpp")
+
+  -- Enable joj package
+  add_rules("joj")
+  set_values("joj.files", "main.cpp")
+  set_values("joj.format", "zip")
+  set_values("joj.sourcedir", "src")
+  set_values("joj.archivedir", "uploads")
+
+target("test")
+  add_packages("conan::boost-ext-ut/1.1.8")
+  set_kind("binary")
+  set_languages("cxx20")
+  add_files("src/*.cpp|main.cpp", "test/main.cpp")
+  add_includedirs("src")
+
+  -- generate coverage report under coverage mode
+  after_run(function (target)
+    if is_mode("coverage") then
+      os.execv("grcov", { ".", "-s", "test", "--binary-path", target:objectdir(), "-t", "html", "--branch", "--ignore-not-existing", "-o", "./coverage" })
+    end
+  end)
